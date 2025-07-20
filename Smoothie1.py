a70369e6120a99202c4e79277bbe833600d311a0
Mani-k-18 <manikantbhajantri18@gmail.com>
2025-07-20 16:22:14 +0530

    Update Smoothie1.py

--- Smoothie1.py
+++ Smoothie1.py
@@ -42,8 +42,8 @@ if Ingredient_list:
    for fruit_chosen in Ingredient_list:
        Ingredient_string += fruit_chosen + ' '
 
-       search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-       st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+      # search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+      # st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
 
    #st.write(Ingredient_string)
 
